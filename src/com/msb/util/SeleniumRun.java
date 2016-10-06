1404a69bde6f563f9348ded30438e9f566fdb825
lavanya56 <pujarilavanya4@gmail.com>
2016-10-06 07:16:58 -0400

    Update SeleniumRun.java

--- src/com/msb/util/SeleniumRun.java
+++ src/com/msb/util/SeleniumRun.java
@@ -25,53 +25,7 @@ public class SeleniumRun {
 			JUnitCore junit = new JUnitCore();
  
 
-// 			junit.run(AccountSearch.class);//done
-		    //junit.run(AddAccount.class);//done
-//			junit.run(EditAccount.class);//done	
-// 			junit.run(EditProfile.class); //done 
-//			junit.run(AssignAccount.class);//done
-//			junit.run(RequirementDashboard.class);//done
-//			junit.run(DashboardProject.class);//done
-//			junit.run(ResetUserPassword.class);	//done
-//			junit.run(ChangeMyPassword.class);//done
-//			junit.run(ActionAuthorization.class); //done
-//			junit.run(HomeRedirect.class);//done
-//			junit.run(AccountUpload.class);//done
-			
-//			junit.run(ContactUpload.class);	//done
-//			junit.run(ContactDetails.class);//done
-//			junit.run(AttachmentDetails.class);//done
-//			junit.run(LocationDetails.class);
-			 //done 
-//	        junit.run(Timesheet.class); //done
-//			junit.run(TaskDetails.class); //done
-//			junit.run(CostCenter.class);//done
-//			junit.run(EmployeeGrouping.class);//done
-//			junit.run(Budget.class);     //done
-//			junit.run(SearchRequirement.class);//done
-//			junit.run(AddRequirement.class);//done
-//			junit.run(EditRequirement.class);//done
-//			junit.run(TechReview.class);//done
-//			junit.run(AddConsultant.class);
-//			junit.run(EditConsultant.class);
-//			junit.run(Invoice.class);
-//			junit.run(Contract.class);
-//			junit.run(ProjectDetails.class); //done
-//			junit.run(VendorRequirement.class);//done
-//			junit.run(Question.class);         //issue
-//			junit.run(UploadQuestion.class);   // issue
-//			junit.run(AdminLogin.class);
-//			junit.run(CustomerAdmin.class);	
-//			junit.run(CustomerEmployee.class);
-//			junit.run(CustomerTeamLead.class);
-//			junit.run(Director.class);
-//			junit.run(Manager.class);
-//			junit.run(ProjectManager.class);
-//			junit.run(VendorAdmin.class);
-//			junit.run(VendorEmployee.class);
-//			junit.run(VendorTeamLead.class);
-//			junit.run(LoggerSearch.class);	
-//			junit.run(CSRSearch.class); //done
+// 		
 			
 			MSBUtils.writeLog("End Time : " + new Date());
 			MSBUtils.displayTableReport();
